4b9cde6d60bd3fecbd2bd2f696cb8fcdf8f68efa
Ivan <mihnenok@gmail.com>
2018-10-13 17:21:43 +0300

    Removed unused functionality

---
+++ show_stabalized_face.py
@@ -0,0 +1,105 @@
+# USAGE
+# python show_stabalized_face.py --detector face_detection_model  --embedding-model openface_nn4.small2.v1.t7
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+#import pickle
+import time
+import cv2
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--detector", required=True,
+	help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m", "--embedding-model", required=True,
+	help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+modelPath = os.path.sep.join([args["detector"],
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# initialize the video stream, then allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+minImgRes = 150
+
+# start the FPS throughput estimator
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream
+	frame = vs.read()
+
+	# resize the frame to have a width of 600 pixels (while
+	# maintaining the aspect ratio), and then grab the image
+	# dimensions
+	# frame = imutils.resize(frame, width=1200)
+	(h, w) = frame.shape[:2]
+
+	# construct a blob from the image
+	imageBlob = cv2.dnn.blobFromImage(
+		cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+	# apply OpenCV's deep learning-based face detector to localize
+	# faces in the input image
+	detector.setInput(imageBlob)
+	detections = detector.forward()
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# extract the face ROI
+			face = frame[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+
+			# ensure the face width and height are sufficiently large
+			if fW > minImgRes or fH > minImgRes:
+				# TODO improve ratio calc
+				stndFace = cv2.resize(face, (300, 400))
+				break
+
+	# update the FPS counter
+	fps.update()
+
+	if (fW > minImgRes and fH > minImgRes):
+		# show the output frame
+		cv2.imshow("Frame", stndFace)
+	
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
