a3e6cb734ae7b0a8f904fd89bda976375aa22ce6
Ivan <mihnenok@gmail.com>
2018-10-13 17:12:53 +0300

    Execution comand updated

--- show_face.py
+++ show_face.py
@@ -1,9 +1,5 @@
 # USAGE
-# python recognize_video.py --detector face_detection_model \
-#	--embedding-model openface_nn4.small2.v1.t7 \
-#	--recognizer output/recognizer.pickle \
-#	--le output/le.pickle
-
+# python show_face.py --detector face_detection_model  --embedding-model openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
