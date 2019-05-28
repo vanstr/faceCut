5563f76d15dd90f4f956ec5e3bcb46b2f0b245fe
Ivan <mihnenok@gmail.com>
2019-05-29 02:22:41 +0300

    - logic for face moving out/in functionality - basic face tracking, starts from center position - removed face proportiional resize

--- show_face.py
+++ show_face.py
@@ -2,6 +2,7 @@
 # python show_face.py --detector face_detection_model
 # import the necessary packages
 import argparse
+import collections
 import ctypes
 import os
 import time
@@ -32,20 +33,18 @@ detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
 # initialize the video stream, then allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-# vs = VideoStream(src=0).start()
-# vs.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-# vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-
 vs = cv2.VideoCapture(0)
 # Check success
 if not vs.isOpened():
     raise Exception("Could not open video device")
-# Set properties. Each returns === True on success (i.e. correct resolution)
-vs.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+faceSearchAreaWidth = 300
+cameraResWidth = 1280
+cameraResHeight = 720
+vs.set(cv2.CAP_PROP_FRAME_WIDTH, cameraResWidth)
+vs.set(cv2.CAP_PROP_FRAME_HEIGHT, cameraResHeight)
 
 time.sleep(2.0)
-
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 screenHeight = user32.GetSystemMetrics(0)
@@ -59,6 +58,71 @@ overlay = cv2.resize(overlay, (screenHeight, screenWidth))
 
 background = cv2.imread('loreta/background.png')
 background = cv2.resize(background, (screenHeight, screenWidth))
+
+centerX = cameraResWidth // 2
+centerY = cameraResHeight // 2
+
+latest_nose_coordinates_x = centerX
+latest_nose_coordinates_y = centerY
+
+min_amount_of_detection_in_quee_to_show_face = 5
+face_statistic_quee = 15
+is_shown_face_model = False
+lastValidFaceRecognition = 0
+face_recognition_stat = collections.deque([], face_statistic_quee)
+face_recognition_stat.appendleft(False)
+
+print("[INFO] Configurations:")
+print("[INFO] - min face detecction = "
+      + str(min_amount_of_detection_in_quee_to_show_face)
+      + ", " + str(face_recognition_stat))
+
+
+#
+# ser = serial.Serial('COM4')
+#
+# if ser.isOpen():
+#     ser.close()
+# ser.open()
+# ser.isOpen()
+
+
+def move_face_model(move_forward):
+    # global ser
+    print("[INFO] move face:" + str(move_forward))
+    # if move_forward:
+    #     ser.write("F\n")
+    # else:
+    #     ser.write("B\n")
+
+
+def update_face_model_state():
+    global face_recognition_stat
+    global is_shown_face_model
+    successful_recognition_amount = list(face_recognition_stat).count(True)
+
+    print("[DEBUG] successfully recognized faces in queue:" + str(successful_recognition_amount))
+    if not is_shown_face_model:
+        if successful_recognition_amount > min_amount_of_detection_in_quee_to_show_face:
+            show_face_model()
+    else:
+        if True not in face_recognition_stat:
+            reset_nose_coordinates()
+            hide_face_model()
+
+
+def show_face_model():
+    global is_shown_face_model
+    is_shown_face_model = True
+    move_face_model(True)
+    print("[DEBUG] Move on face model...")
+
+
+def hide_face_model():
+    global is_shown_face_model
+    is_shown_face_model = False
+    move_face_model(False)
+    print("[DEBUG] Hide face model...")
 
 
 def display_face_frame(face):
@@ -87,51 +151,126 @@ def get_biggest_face_coordinates(detections, w, h):
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             detection_width = endX - startX
-            if biggest_width < detection_width:
+            detection_height = endY - startY
+            if biggest_width < detection_width and detection_height > detection_width / 2:
                 biggest_width = detection_width
                 biggest_rect = (startX, startY, endX, endY)
     return biggest_rect
 
 
-def get_adapted_face(rect):
+def get_adapted_face(rect, face_searching_frame):
     (startX, startY, endX, endY) = rect
-    nose_coordinates = (float(startX + endX) / 2, float(startY + endY) / 2)
+    # w = endX - startX
+    # h = w / 9 * 16
+    # startY = int(nose_coordinates[1] - float(h / 2))
+    # endY = int(nose_coordinates[1] + float(h / 2))
 
-    w = endX - startX
-    h = w / 9 * 16
-    startY = int(nose_coordinates[1] - float(h / 2))
-    endY = int(nose_coordinates[1] + float(h / 2))
+    return face_searching_frame[startY:endY, startX:endX]
 
-    face = frame[startY:endY, startX:endX]
-    return face
+
+def updateFoundFaceStat(detections):
+    global face_recognition_stat
+    print("[DEBUG] is_found_face amount:" + str(len(detections)))
+    if len(detections) > 0:
+        face_recognition_stat.appendleft(True)
+        return True
+    else:
+        face_recognition_stat.appendleft(False)
+        return False
+
+
+def show_face_of_frame(face_searching_frame):
+    # construct a blob from the image
+    imageBlob = cv2.dnn.blobFromImage(
+        cv2.resize(face_searching_frame, (300, 300)), 1.0, (300, 300),
+        (104.0, 177.0, 123.0), swapRB=False, crop=False)
+    # apply OpenCV's deep learning-based face detector to localize
+    # faces in the input image
+    detector.setInput(imageBlob)
+    detections = detector.forward()
+    (h, w) = face_searching_frame.shape[:2]
+    rect = get_biggest_face_coordinates(detections, w, h)
+    if rect is not None:
+        # extract the face ROI
+        face = get_adapted_face(rect, face_searching_frame)
+        (fH, fW) = face.shape[:2]
+
+        # ensure the face width and height are sufficiently large
+        if fW > 0 and fH > 0:
+            face_recognition_stat.appendleft(True)
+            if is_shown_face_model:
+                display_face_frame(face)
+            updateNoseCoordinates(rect)
+
+        else:
+            face_recognition_stat.appendleft(False)
+            print("[DEBUG] face to small "
+                  + str(fW) + ":" + str(fH)
+                  + str(rect)
+                  + " expected at least " + str(args["minimgwidth"])
+                  )
+    else:
+        face_recognition_stat.appendleft(False)
+
+    update_face_model_state()
+
+    if not is_shown_face_model and args["fullscreen"]:
+        blank_image = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+        cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow("Frame", blank_image)
+
+
+def updateNoseCoordinates(rect):
+    global latest_nose_coordinates_x
+    global latest_nose_coordinates_y
+    (startX, startY, endX, endY) = rect
+    if startY > 0 and startX > 0 and endY - startY <= faceSearchAreaWidth and endX - startX <= faceSearchAreaWidth:
+        nose_coordinates = (float(startX + endX) / 2, float(startY + endY) / 2)
+        latest_nose_coordinates_x = latest_nose_coordinates_x - faceSearchAreaWidth / 2 + int(
+            nose_coordinates[0])
+        latest_nose_coordinates_y = latest_nose_coordinates_y - faceSearchAreaWidth / 2 + int(
+            nose_coordinates[1])
+        print("[INFO] Updated nose coordinates to "
+              + str(latest_nose_coordinates_x) + ":" + str(latest_nose_coordinates_y))
+
+
+def reset_nose_coordinates():
+    global latest_nose_coordinates_x
+    global latest_nose_coordinates_y
+    print("[INFO] Reset nose coordinates to center")
+
+    latest_nose_coordinates_x = centerX
+    latest_nose_coordinates_y = centerY
+
+
+def get_face_tracked_area():
+    startX = latest_nose_coordinates_x - faceSearchAreaWidth / 2
+    startY = latest_nose_coordinates_y - faceSearchAreaWidth / 2
+
+    if startX < 0:
+        startX = 0
+    endX = startX + faceSearchAreaWidth
+    if endX > screenWidth:
+        endX = screenWidth
+
+    if startY < 0:
+        startY = 0
+    endY = startY + faceSearchAreaWidth
+    if endY > screenHeight:
+        endY = screenHeight
+
+    return frame[startY:endY, startX:endX]
 
 
 # loop over frames from the video file stream
 while True:
     # grab the frame from the threaded video stream
     rate, frame = vs.read()
-    (h, w) = frame.shape[:2]
-    print("[INFO] - img width, " + str(w))
-    # construct a blob from the image
-    imageBlob = cv2.dnn.blobFromImage(
-        cv2.resize(frame, (300, 300)), 1.0, (300, 300),
-        (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-    # apply OpenCV's deep learning-based face detector to localize
-    # faces in the input image
-    detector.setInput(imageBlob)
-    detections = detector.forward()
-
-    rect = get_biggest_face_coordinates(detections, w, h)
-    if rect is not None:
-
-        # extract the face ROI
-        face = get_adapted_face(rect)
-        (fH, fW) = face.shape[:2]
-
-        # ensure the face width and height are sufficiently large
-        if (fW > 0 and fH > 0) and fW > args["minimgwidth"]:
-            display_face_frame(face)
+    focused_frame = get_face_tracked_area()
+    cv2.imshow("focused_frame", focused_frame)
+    show_face_of_frame(focused_frame)
 
     # update the FPS counter
     fps.update()
